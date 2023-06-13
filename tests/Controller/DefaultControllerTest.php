f53fbb5deeebe7adf444691d518e547cea46d8d5
Josselin Crenn <josselincrenn@gmail.com>
2023-06-13 14:29:12 +0200

    form creta new user with choose role -> OK; 2 tests: defaultController + UserController -> OK + a few css

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -1,18 +1,23 @@
 <?php
 
-namespace Tests\AppBundle\Controller;
+namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testHomePage()
     {
+        //créer une instance  du client de test
         $client = static::createClient();
 
+        //requête client
         $crawler = $client->request('GET', '/');
 
+        //vérifie que la réponse HTTP a un code de statut 200, ce qui signifie que la requête a réussi et que la page a été trouvée.
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+
+        //vérifie que le nombre d'éléments <h1> dans la page est égal à 1. Cela garantit que le titre principal est présent sur la page.
+        $this->assertCount(1, $crawler->filter('h1'));
     }
 }
